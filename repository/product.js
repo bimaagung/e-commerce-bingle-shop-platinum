fffdb40df882d2751a3c3cd30c10a8ba6ea49293
irvan taufik <irvantaufik28@gmail.com>
2022-09-14 23:02:29 +0700

    undo file delete conflict data error

--- repository/product.js
+++ repository/product.js
@@ -1,35 +1,75 @@
-<<<<<<< HEAD
 const {Product} = require('../models')
 
-class ProductRepository {
+class productRepository {
   constructor() {
-    this.ProductModel = Product
+    this.productModel = Product
   }
 
-  async getAllProduct(category_id) {
-    if (category_id != null) {
-      return await this.ProductModel.findAll({
+  async getAllproducts() {
+    let data = null
+    try {
+      
+      data =await this.productModel.findAll()
+    } catch (e) {
+      console.log(e)
+      return null
+    }
+    return data
+  }
+
+  async getproductByID(id) {
+    let data = null
+    try {
+      data = await this.productModel.findOne({
         where: {
-          category_id: category_id
+          id: id,
         },
       })
+    } catch (err) {
+      console.log(err)
+      return null
     }
-
-    return await this.ProductModel.findAll()
-=======
-class ProductUC {
-  constructor(productRepository) {
-    this.productRepository = productRepository
->>>>>>> 8c7fa5d130ad70f64ea99382671359ff2e9e7cc1
+    return data
   }
 
-  async getProductByID(id) {
-    return await this.productRepository.getProductByID(id)
+  async addProduct(product) {
+    let data = null
+    try {
+      data = await this.productModel.create(product)
+    } catch (err) {
+      console.log(err)
+      return null
+    }
+    return data
   }
-
-  async getAllProduct(filters) {
-    return await this.productRepository.getAllProduct(filters)
+  async updateProduct(id,product) {
+    let data = null
+    try {
+      data = await this.productModel.update(product,{
+        where: {
+          id: id,
+        },
+      })
+    } catch (err) {
+      console.log(err)
+      return null
+    }
+    return data
+  }
+  async deleteProduct(id) {
+    let data = null
+    try {
+      data = await this.productModel.destroy({
+        where: {
+          id: id,
+        },
+      })
+    } catch (err) {
+      console.log(err)
+      return null
+    }
+    return data
   }
 }
 
-module.exports = ProductUC
+module.exports = productRepository
