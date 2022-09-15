1145f281ddf45d374a5147ebc0f9ca99b01606d5
bimaagung <bimaagung.labs@gmail.com>
2022-09-15 09:05:48 +0700

    preparation feature order

--- controller/category.js
+++ controller/category.js
@@ -1,10 +1,11 @@
 const resData = require('../helper/response')
 module.exports = {
-    
   getAllCategory: async (req, res) => {
     let category = await req.categoryUC.getAllCategory(null)
     if (category == null) {
-     return res.status(404).json(resData.failed("failed, category not found", null))
+      return res
+        .status(404)
+        .json(resData.failed('failed, category not found', null))
     }
     return res.status(200).json(resData.success(category))
   },
@@ -13,37 +14,44 @@ module.exports = {
     let id = req.params.id
     let category = await req.categoryUC.getCategoryByID(id)
     if (category == null) {
-      return res.status(400).json(resData.failed("failed, category not found", null))
+      return res
+        .status(400)
+        .json(resData.failed('failed, category not found', null))
     }
     return res.status(200).json(resData.success(category))
-    
   },
-    addCategory: async (req, res) => {
-      let category = req.body.name
-      let update_category = await req.categoryUC.addCategory(category)
-      if (update_category == null) {
-        return res.status(400).json(resData.failed("failed, category not found", null))
-      }
-      return res.status(200).json(resData.success(category))
-    },
 
-    putCategory: async (req, res) => {
-      let category = req.body.name
-      let id = req.params.id
-      let update_category = await req.CategoryUC.putCategory(id,category)
-      if (update_category == null) {
-        return res.status(400).json(resData.failed("failed, category not found", null))
-      }
-      return res.status(200).json(resData.success(category))
-    },
+  addCategory: async (req, res) => {
+    let category = req.body.name
+    let update_category = await req.categoryUC.addCategory(category)
+    if (update_category == null) {
+      return res
+        .status(400)
+        .json(resData.failed('failed, category not found', null))
+    }
+    return res.status(200).json(resData.success(category))
+  },
 
-    deleteCategory: async (req, res) => {
-      let id = req.params.id
-      let category = await req.CategoryUC.deleteCategory(id)
-      if (category == null) {
-        return res.status(400).json(resData.failed("failed, category not found", null))
-      }
-      return res.status(200).json(resData.success(category))
-    },
-  }
-  
+  putCategory: async (req, res) => {
+    let category = req.body.name
+    let id = req.params.id
+    let update_category = await req.CategoryUC.putCategory(id, category)
+    if (update_category == null) {
+      return res
+        .status(400)
+        .json(resData.failed('failed, category not found', null))
+    }
+    return res.status(200).json(resData.success(category))
+  },
+
+  deleteCategory: async (req, res) => {
+    let id = req.params.id
+    let category = await req.CategoryUC.deleteCategory(id)
+    if (category == null) {
+      return res
+        .status(400)
+        .json(resData.failed('failed, category not found', null))
+    }
+    return res.status(200).json(resData.success(category))
+  },
+}
