2ea6a8d75e38dc9b5074d138038bf316b18a1652
bimaagung <bimaagung.labs@gmail.com>
2022-09-21 11:08:09 +0700

    add form validation in auth

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,9 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../controller/auth_controller');
 const handleUpload = require('../libs/handle_upload');
+const validation = require('../middleware/formValidation');
 
-router.post('/api/user/login', auth.login);
-router.post('/api/user/register', handleUpload.upload.single('image'), auth.register);
+router.post('/api/user/login', validation.login, auth.login);
+router.post('/api/user/register', validation.register, handleUpload.upload.single('image'), auth.register);
 
 module.exports = router;
