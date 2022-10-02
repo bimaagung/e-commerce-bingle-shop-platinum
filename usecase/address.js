98ae96d82c5e9ad32fa85dd86b8312186cda61ce
faris <frsakbr@gmail.com>
2022-10-02 15:58:58 +0700

    error recode address

--- usecase/address.js
+++ usecase/address.js
@@ -3,24 +3,104 @@ class AddressUC {
     this.AddressRepository = addressRepository;
   }
 
-  async getAddressByID(id) {
-    return await this.AddressRepository.getAddressByID(id);
+  async getAllAddress(id) {
+    let result = {
+      isSuccess: false,
+      reason: '',
+      data: null,
+    };
+
+    let getAllAddress = await this.AddressRepository.getAllAddress(id);
+
+    if (getAllAddress == null) {
+      result.reason = 'list is empty';
+    };
+
+    result.isSuccess = true;
+    result.data = getAllAddress;
+
+    return result;
   }
 
+
   async getAllAddress(userId) {
-    return await this.AddressRepository.getAllAddress(userId);
+    let result = {
+      isSuccess: false,
+      reason: '',
+      data: null,
+    };
+
+    let getAllAddress = await this.AddressRepository.getAllAddress(userId);
+
+    if (getAllAddress == null) {
+      result.reason = 'list is empty';
+    };
+
+    result.isSuccess = true;
+    result.data = getAllAddress;
+
+    return result;
   }
 
   async addAddress(address) {
-    return await this.AddressRepository.addAddress(address);
+    let result = {
+      isSuccess: false,
+      reason: '',
+      data: null,
+    };
+
+    let existUserById = await this.UserRepository.getUserById(address.user_id);
+
+    if (existUserById == null) {
+      result.reason = 'user id not found';
+    };
+
+    // memasukkan address ke database
+    let addAddress = await this.AddressRepository.addAddress(address);
+
+    result.isSuccess = true;
+    result.data = addAddress;
+    return result;
   }
 
   async updateAddress(id, address) {
-    return await this.AddressRepository.updateAddress(id, address);
+    let result = {
+      isSuccess: false,
+      reason: '',
+      data: null,
+    };
+
+    let existAddress = await req.addressUC.getAddressByID(id);
+
+    if (existAddress == null) {
+      result.reason = 'address id not found';
+    };
+
+    let updateAddress = await this.AddressRepository.updateAddress(id, address);
+
+    result.isSuccess = true;
+    result.data = updateAddress;
+    return result;
   }
 
   async deleteAddress(id) {
-    return await this.AddressRepository.deleteAddress(id);
+    let result = {
+      isSuccess: false,
+      reason: '',
+      data: null,
+    };
+
+    let existAddress = await req.addressUC.getAddressByID(id);
+
+    if (existAddress == null) {
+      result.reason = 'address id not found';
+    };
+
+    let deleteAddress = await this.AddressRepository.deleteAddress(id);
+
+    result.isSuccess = true;
+    result.data = deleteAddress;
+    return result;
   }
 }
 module.exports = AddressUC;
