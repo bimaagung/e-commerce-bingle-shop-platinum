fc9bc4e93da95772c898862c154c4b87a1395f44
bimaagung <bimaagung.labs@gmail.com>
2022-10-06 14:37:18 +0700

    fixing validation image in register

--- middleware/formValidation.js
+++ middleware/formValidation.js
@@ -7,7 +7,7 @@ module.exports = {
     const response = Joi.object({
       name: Joi.string().required(),
       username: Joi.string().required(),
-      image : Joi.string().allow(null),
+      image : Joi.string().allow(null).allow(''),
       telp: Joi.number().required(),
       password: Joi.string().min(6),
       confrimPassword: Joi.ref('password'),
