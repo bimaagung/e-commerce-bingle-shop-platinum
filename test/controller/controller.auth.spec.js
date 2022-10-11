8e492f63836d1ce3bc70227bebd8b2f41524a804
irvan taufik <irvantaufik28@gmail.com>
2022-10-11 11:32:48 +0700

    checkout

--- test/controller/controller.auth.spec.js
+++ test/controller/controller.auth.spec.js
@@ -1,39 +1,40 @@
 const authController = require('../../controller/auth')
 const resData = require('../../helper/response')
+const bcrypt = require('bcrypt')
 
 let mockAuthUC = {
     login: jest.fn().mockReturnValue(null),
-    register : jest.fn().mockReturnValue(null)
+    register: jest.fn().mockReturnValue(null)
 }
 
-const mockRequest = (body={}, params={}, user={}, useCase={})=>{
+const mockRequest = (body = {}, params = {}, user = {}, useCase = {}) => {
     return {
-        body : body,
-        params : params,
-        user : user,
+        body: body,
+        params: params,
+        user: user,
         ...useCase
     }
 }
 
-const mockResponse = ()=>{
+const mockResponse = () => {
     const res = {}
     res.status = jest.fn().mockReturnValue(res)
     res.json = jest.fn().mockReturnValue(res)
 
     return res
 }
-const next = (error)=>{
+const next = (error) => {
     console.log(error.message)
 }
 
-describe('test login', ()=>{
-    describe('login success', ()=>{
+describe('test login', () => {
+    describe('login success', () => {
         const user = {
             id: 9,
             name: "irvan",
             username: "irvan28",
             image: null,
-            password : "password",
+            password: "password",
             telp: "082311113",
             is_admin: false,
             email: "irvan@gmail.com",
@@ -41,23 +42,75 @@ describe('test login', ()=>{
             updatedAt: "2022-10-06T07:04:51.142Z"
         }
 
-        
-        test('return status 200 isSuccess true', async ()=>{
+
+        test('return status 200 isSuccess true', async () => {
             mockAuthUC.login = jest.fn().mockReturnValue(
-                {isSuccess: true, reason:"", data : user}
+                { isSuccess: true, reason: "", data: user }
             )
             let req = mockRequest(
-                {username : "irvan", password :"password"},
-                {},{},{authUC : mockAuthUC}
-                )
+                { username: "irvan", password: "password" },
+                {}, {}, { authUC: mockAuthUC }
+            )
 
             let res = mockResponse()
 
             await authController.login(req, res, next)
-            
+
             expect(mockAuthUC.login).toHaveBeenCalled()
             expect(res.status).toBeCalledWith(200)
+        })
+        test('return status 404 isSucces false', async () => {
+            mockAuthUC.login = jest.fn().mockReturnValue(
+                { isSuccess: false, reason: "username or email not aviable", data: null }
+            )
+            let req = mockRequest(
+                { username: "worngUsername", password: "wrongPassword" },
+                {}, {}, { authUC: mockAuthUC }
+            )
+            let res = mockResponse()
+            await authController.login(req, res, next)
+
+            expect(mockAuthUC.login).toHaveBeenCalled()
+            expect(res.json).toBeCalledWith(resData.failed('username or email not aviable'));
+        })
     })
+    describe('test register', () => {
+        describe('register success', () => {
+            const user = {
+                name: "irvan",
+                username: "irvan28",
+                image: null,
+                password: bcrypt.hashSync('123456', 10),
+                telp: "082311113",
+                is_admin: false,
+                email: "irvan@gmail.com",
+            }
+            test('return status 200 isSuccess true', async () => {
+                mockAuthUC.register = jest.fn().mockReturnValue(
+                    { isSuccess: true, reason: "", data: user }
+                )
+                let req = mockRequest(
+                    {
+                        name: "irvan",
+                        username: "irvan28",
+                        image: null,
+                        password: bcrypt.hashSync('123456', 10),
+                        telp: "082311113",
+                        is_admin: false,
+                        email: "irvan@gmail.com"
+                    },
+                    {}, {}, { authUC: mockAuthUC }
+                )
+
+                let res = mockResponse()
+                console.log(res)
+
+                await authController.register(req, res, next)
+
+                // expect(mockAuthUC.register).toHaveBeenCalled()
+                expect(res.status).toBeCalledWith(200)
+            })
+        })
     })
 })
 
