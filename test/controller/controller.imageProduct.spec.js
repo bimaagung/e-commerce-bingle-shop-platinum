cfcf857c9a2aa285c6966474ca5352e9ec8a4501
irvan taufik <irvantaufik28@gmail.com>
2022-10-14 18:00:52 +0700

    error test

--- test/controller/controller.imageProduct.spec.js
+++ test/controller/controller.imageProduct.spec.js
@@ -3,17 +3,18 @@ const resData = require("../../helper/response");
 
 let mockImageProductUC = {
   getImageProductByProductID: jest.fn().mockReturnValue(null),
-  addProductImag: jest.fn().mockReturnValue(null),
+  addProductImage: jest.fn().mockReturnValue(null),
   createImageProduct: jest.fn().mockReturnValue(null),
 //   updateImageProduct: jest.fn().mockReturnValue(null),
 //   deleteImageProduct: jest.fn().mockReturnValue(null),
 };
 
-const mockRequest =(body={}, query={}, params={}, useCases={}) => {
+const mockRequest =(body={}, query={}, params={},product_id={}, useCases={}) => {
     return {
         body: body,
         query: query,
         params: params,
+        product_id : product_id,
         ...useCases
     }
 }
@@ -31,15 +32,15 @@ const next = (error) =>{
 describe("Test upload image", () => {
   describe("createImage test", () => {
     const imageProduct = {
-      id: 10,
+      // id: 10,
       url: "http://res.cloudinary.com/dnvltueqb/image/upload/v1665724533/product/1665724539897_data_14_background-zoom-rumah-spongebob-19_mxqzsa.jpg",
       product_id: 3,
       updatedAt: "2022-10-14T05:15:42.095Z",
       createdAt: "2022-10-14T05:15:42.095Z",
     };
 
-    test("should statu 200 isSucces = true", async () => {
-      mockImageProductUC.createImageProduct = jest.fn().mockReturnValue({ isSuccess: false, reason: "", data: imageProduct });
+    test("should status 200 isSucces = true", async () => {
+      mockImageProductUC.addProductImage = jest.fn().mockReturnValue({ isSuccess: false, reason: "", data: imageProduct });
       let req = mockRequest({},{},{},{},{ productImageUC:mockImageProductUC })
       let res = mockResponse();
 
@@ -65,7 +66,7 @@ describe("Test upload image", () => {
             mockImageProductUC.getImageProductByProductID = jest.fn().mockReturnValue(
                 {isSuccess : true, reason:null , data : []}
             )
-          let req = mockRequest({product_id : 3},{}, {}, {
+          let req = mockRequest({product_id : 3},{}, {},{}, {
             productImageUC: mockImageProductUC
         })
         let res = mockResponse()
