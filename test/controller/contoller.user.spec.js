bb85e00c19acc5ca50db57d7ef58d09987af90d9
bimaagung <bimaagung.labs@gmail.com>
2022-10-15 10:32:52 +0700

    add testing controller user 'getOneUser'

--- test/controller/contoller.user.spec.js
+++ test/controller/contoller.user.spec.js
@@ -37,7 +37,99 @@ const next = () => jest.fn().mockReturnValue(
 );
 
 describe('Test User', () => {
-    describe('getOneUser', () => {
-        test        
+    describe('getOneUser test', () => {
+
+        const user = {
+            name: 'user',
+            username: 'user1',
+            image: 'http://localhost:8080/images/user1.jpg',
+            telp: '0847383672',
+            email: 'user@example.com',
+        }
+        test('should status is 200 and type data is array', async () => {
+              mockUserUC.getUserByID = jest.fn().mockReturnValue(
+                {isSuccess: true, reason:null, data: user, statusCode: 200}
+            );
+            
+            let req = mockRequest({},{},{},{id:1},{ userUC: mockUserUC });
+            let res = mockResponse();
+
+             await userController.getOneUser(req, res, next);
+
+            expect(mockUserUC.getUserByID).toHaveBeenCalled();
+            expect(res.status).toBeCalledWith(200)
+            expect(res.json).toBeCalledWith(resData.success(user));
+        });
+
+        test('should status is 404 and message is "user not found"', async () => {
+              mockUserUC.getUserByID = jest.fn().mockReturnValue(
+                {isSuccess: false, reason:'user not found', data: null, statusCode: 404}
+            );
+            
+            let req = mockRequest({},{},{},{id:2},{ userUC: mockUserUC });
+            let res = mockResponse();
+
+             await userController.getOneUser(req, res, next);
+
+            expect(mockUserUC.getUserByID).toHaveBeenCalled();
+            expect(res.status).toBeCalledWith(404)
+            expect(res.json).toBeCalledWith(resData.failed('user not found', null));
+        });
+
+        test("should status is 500 and message is 'internal server error'", async () => {
+            mockUserUC.getUserByID = jest.fn().mockImplementation(() => {
+                throw new Error();
+            });
+
+            let req = mockRequest({},{},{},{id:1},{ userUC: mockUserUC });
+            let res = mockResponse();
+            let serverError = next();
+
+            await userController.getOneUser(req, res, next);
+
+            expect(serverError().status).toEqual(500);
+            expect(serverError().json.message).toEqual('internal server error');
+        });
+    });
+    
+    describe('updateUser test', () => {
+
+        const user = {
+            name: 'user',
+            username: 'user1',
+            image: 'http://localhost:8080/images/user1.jpg',
+            telp: '0847383672',
+            email: 'user@example.com',
+        }
+
+        test('should status is 200', async () => {
+              mockUserUC.getUserByID = jest.fn().mockReturnValue(
+                {isSuccess: true, reason:null, data: user, statusCode: 200}
+            );
+            
+            let req = mockRequest({},{},{},{id:1},{ userUC: mockUserUC });
+            let res = mockResponse();
+
+             await userController.getOneUser(req, res, next);
+
+            expect(mockUserUC.getUserByID).toHaveBeenCalled();
+            expect(res.status).toBeCalledWith(200)
+            expect(res.json).toBeCalledWith(resData.success(user));
+        });
+
+        test('should status is 404 and message is "user not found"', async () => {
+              mockUserUC.getUserByID = jest.fn().mockReturnValue(
+                {isSuccess: false, reason:'user not found', data: null, statusCode: 404}
+            );
+            
+            let req = mockRequest({},{},{},{id:2},{ userUC: mockUserUC });
+            let res = mockResponse();
+
+             await userController.getOneUser(req, res, next);
+
+            expect(mockUserUC.getUserByID).toHaveBeenCalled();
+            expect(res.status).toBeCalledWith(404)
+            expect(res.json).toBeCalledWith(resData.failed('user not found', null));
+        });
     });
 });
