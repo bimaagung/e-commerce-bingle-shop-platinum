3a6fd452cb8d9d65c8aa44b59a11ec8ec8523966
BenaDasBInIch <105765491+BMHR50@users.noreply.github.com>
2022-10-20 21:02:00 +0700

    middleware socket.io function authorize

--- middleware/socket_io.js
+++ middleware/socket_io.js
@@ -11,6 +11,26 @@ function getToken(authHeader) {
   
     return splitHeader[0];
   }
+
+function authorize( socket, next) {
+    let authHeader = socket.handshake.headers['authorization']
+    if(typeof authHeader !== "string") {
+        return next(new Error(failedMsg))
+    }    
+    let token = getToken(authHeader)
+    let payload = null;
+
+    payload = jwt.verify(token, process.env.JWT_KEY_SECRET);
+
+    if (payload.is_admin !== isAdmin) {
+      return next(new Error(failedMsg));
+    }
+    let auth = {
+        user: payload.user,
+        is_admin: payload.is_admin
+    }
+}   
+    
   
   
 
