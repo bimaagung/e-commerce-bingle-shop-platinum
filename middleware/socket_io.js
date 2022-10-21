02d6ae0457162a11fad5ed7314c9a180e596207b
bimaagung <bimaagung.labs@gmail.com>
2022-10-21 09:07:02 +0700

    fixing chat

--- middleware/socket_io.js
+++ middleware/socket_io.js
@@ -1,39 +1,38 @@
 const jwt = require('jsonwebtoken');
 
-const failedMsg = 'socket authorization failed'
+const FAILED_MESSAGE = 'socket authorization failed';
 
 function getToken(authHeader) {
-    let splitHeader = authHeader.split(' ');
-  
-    if (splitHeader.length > 1) {
-      return splitHeader[1];
-    }
-  
-    return splitHeader[0];
+  let splitHeader = authHeader.split(' ');
+
+  if (splitHeader.length > 1) {
+    return splitHeader[1];
   }
 
-function authorize( socket, next) {
-    let authHeader = socket.handshake.headers['authorization']
-    if(typeof authHeader !== "string") {
-        return next(new Error(failedMsg))
-    }    
-    let token = getToken(authHeader)
-    let payload = null;
+  return splitHeader[0];
+}
 
+function authorize(socket, next) {
+  let authHeader = socket.handshake.headers.authorization;
+  if (typeof authHeader !== 'string') {
+    return next(new Error(FAILED_MESSAGE));
+  }
+
+  let token = getToken(authHeader);
+  let payload = null;
+
+  try {
     payload = jwt.verify(token, process.env.JWT_KEY_SECRET);
+  } catch (error) {
+    return next(new Error(FAILED_MESSAGE));
+  }
 
-    if (payload.is_admin !== isAdmin) {
-      return next(new Error(failedMsg));
-    }
-    let auth = {
-        user: payload.user,
-        is_admin: payload.is_admin
-    }
-    socket.handshake.auth = auth
-    next()
-}   
-    
-module.exports = authorize
-  
+  let auth = {
+    user: payload,
+    is_admin: payload.is_admin,
+  };
+  socket.handshake.auth = auth;
+  next();
+}
 
-  
+module.exports = authorize;
