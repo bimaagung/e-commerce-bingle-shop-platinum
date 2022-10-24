3f8511e48bcacd3695fc398c3b8b9f8a6d5b14eb
bimaagung <bimaagung.labs@gmail.com>
2022-10-24 16:39:09 +0700

    add port in db

--- config/config.js
+++ config/config.js
@@ -6,6 +6,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOSTNAME || 'localhost',
+    port: process.env.DB_PORT || 5432, 
     dialect: process.env.DB_DIALECT,
   },
 };
